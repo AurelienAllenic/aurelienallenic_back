4ddeb672b231b6c1cc761a1256277427b09a7f2a
AurelienAllenic <aurelien.allenic@gmail.com>
2024-11-29 16:42:04 +0100

    (feat) added incrementor

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000; // Utiliser la variable d'environnement PORT ou 3000 par défaut
 
+// Initialisation du compteur pour les clics sur le QR code
+let qrClickCount = 0;
+
 app.use(cors({
     origin: ['https://aurelienallenic.fr', 'http://localhost:5173', 'http://localhost:5174'],
 }));
@@ -23,10 +26,24 @@ app.post('/send-email', async (req, res) => {
         res.status(500).json({ success: false });
     }
 });
+
+// Route pour incrémenter le compteur des clics sur le QR code
+app.get('/increment-qr', (req, res) => {
+    qrClickCount++; // Incrémente le compteur
+    console.log(`QR code clicked ${qrClickCount} times`);
+    res.status(200).json({ success: true, clickCount: qrClickCount });
+});
+
+// Route pour vérifier le nombre de clics
+app.get('/check-qr-clicks', (req, res) => {
+    res.status(200).json({ clickCount: qrClickCount });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  });
+});
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
+
