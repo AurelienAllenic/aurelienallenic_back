446176b98c2c6b205ffc8c32134d97d97264314a
AurelienAllenic <aurelien.allenic@gmail.com>
2024-12-22 17:03:45 +0100

    (fix) updated cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -13,4 +13,18 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-module.exports = cors(corsOptions);
+// Middleware CORS
+const corsConfig = (req, res, next) => {
+    cors(corsOptions)(req, res, () => {
+        // Gérer les pré-vols (OPTIONS) ici
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            return res.status(200).send();
+        }
+        next();
+    });
+};
+
+module.exports = corsConfig;
