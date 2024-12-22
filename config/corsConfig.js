034c9e79d3e6eef88bd71ce42b6597c7328ac9be
AurelienAllenic <aurelien.allenic@gmail.com>
2024-12-22 17:07:46 +0100

    (fix) updated cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,14 +1,24 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: [
-        'https://aurelienallenic.fr',
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'https://paro-officiel.com',
-        'https://paro-musique.com',
-    ],
-    credentials: true,
+    origin: (origin, callback) => {
+        // Liste des origines autorisées
+        const allowedOrigins = [
+            'https://aurelienallenic.fr',
+            'http://localhost:5173',
+            'http://localhost:5174',
+            'https://paro-officiel.com',
+            'https://paro-musique.com'
+        ];
+
+        // Vérifier si l'origine de la requête est dans la liste des origines autorisées
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true); // Accepte la requête
+        } else {
+            callback(new Error('CORS policy: Origin not allowed'), false); // Refuse la requête
+        }
+    },
+    credentials: true, // Permet d'envoyer des cookies si nécessaire
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
@@ -16,9 +26,7 @@ const corsOptions = {
 // Middleware CORS
 const corsConfig = (req, res, next) => {
     cors(corsOptions)(req, res, () => {
-        // Gérer les pré-vols (OPTIONS) ici
         if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
             return res.status(200).send();
