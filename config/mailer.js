481ccdd0443245fdd7aa935fb6b63aef61a15939
AurelienAllenic <aurelien.allenic@gmail.com>
2024-12-24 16:16:46 +0100

    (fix) corrected nodemailer

--- config/mailer.js
+++ config/mailer.js
@@ -1,15 +1,23 @@
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    seucre: true,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.SMTP_EMAIL,
+        pass : process.env.SMTP_PASSWORD,
     },
+    port: 465,
+    host: "smtp.gmail.com",
 });
 
 const sendEmail = async (to, subject, text) => {
-    const mailOptions = { from: process.env.EMAIL_USER, to, subject, text };
+    console.log(`${to}, ${subject}, ${text}`);
+    console.log('SMTP_EMAIL:', process.env.SMTP_EMAIL);
+console.log('SMTP_PASSWORD:', process.env.SMTP_PASSWORD);
+    const mailOptions = { from: process.env.SMTP_EMAIL, to, subject, text };
     await transporter.sendMail(mailOptions);
 };
 
