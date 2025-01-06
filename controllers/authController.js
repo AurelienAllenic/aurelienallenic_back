0dfff2a2349ea15f42c2329048d6ec90679e4a43
AurelienAllenic <aurelien.allenic@gmail.com>
2025-01-06 10:14:59 +0100

    (fix) added valid true to the verify token controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,16 +65,16 @@ exports.verifyToken = (req, res) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: 'Token manquant ou invalide.' });
+        return res.status(401).json({ message: 'Token manquant ou invalide.', isValid: false });
     }
 
     const token = authHeader.split(" ")[1];
 
     try {
         const decodedToken = jwt.verify(token, process.env.RANDOM_SECRET_TOKEN || 'secret_key');
-        res.status(200).json({ message: 'Token valide.', userId: decodedToken.userId });
+        res.status(200).json({ isValid: true, message: 'Token valide.', userId: decodedToken.userId });
     } catch (error) {
         console.error('Erreur lors de la vérification du token:', error);
-        res.status(401).json({ message: 'Token invalide.' });
+        res.status(401).json({ isValid: false, message: 'Token invalide.' });
     }
 };
